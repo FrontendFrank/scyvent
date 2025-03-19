7229fb7f66ad3b19eff3dcc50fab68c5f31b0b0e
FrontendFrank <hoffmann-lange@gmx.net>
2025-03-19 16:48:16 +0100

    first route to board added (wip)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,5 @@
 import { Routes } from '@angular/router';
-export const routes: Routes = [];
+import {BoardComponent} from "./components/board/board.component";
+export const routes: Routes = [
+	{ path: 'board', component: BoardComponent }
+];
